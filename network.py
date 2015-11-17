c7a3b4572f1b8d73fe9c53fadd13d8ead1a64cdf
Rachel Ehrlich <rae84@cornell.edu>
2015-11-17 12:46:44 -0500

    add feedforward to network

--- network.py
+++ network.py
@@ -8,3 +8,10 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[1:], sizes[1:])]
 
+    def feedforward(self, a):
+        """Return the output of the network if "a" is the input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+
+def sigmoid(z):
+    return 1.0/(1.0 + np.exp(-z))
