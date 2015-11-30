1b53c7d29da9d0ee39318c6960a03f29131f9ea7
ack8006 <takata.alex@gmail.com>
2015-11-30 15:04:02 -0500

    my network

---
+++ Network.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class NeutralNetwork(object):
+    def __init__(self):
+        pass
+
+    def cost_function(training, w, b):
+
+
+    def sigmoid(z):
+        return 1/(1+np.exp(-z))
