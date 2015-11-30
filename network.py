d3dd1a78f940600141136e5365cbb2b77d1a4886
ack8006 <takata.alex@gmail.com>
2015-11-30 15:41:13 -0500

    cost

--- network.py
+++ network.py
@@ -8,8 +8,17 @@ class Network(object):
         self.biases = [np.random.randn(y,1) for y in sizes[1:]]
         self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
 
-    def cost_function(training, w, b):
-        pass
+    def cost_function(self, training):
+        return sum((x[1]- self.predict(x[0])**2) for x in training)/(2*len(training))
 
-    def sigmoid(z):
-        return 1/(1+np.exp(-z))
+    def predict(self, x):
+        #self.biases
+        #self.weights
+        for b, w in zip(self.biases, self.weights):
+            x = sigmoid(np.dot(w,x)+b)
+        return x
+
+
+
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
