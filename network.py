7a66849e8c2f15ecdcd69ed33f8c34cb10328714
Rachel Ehrlich <rae84@cornell.edu>
2015-12-14 16:29:26 -0500

    add feedforward

--- network.py
+++ network.py
@@ -3,11 +3,11 @@ import numpy as np
 import scipy.io
 
 class Network(object):
-    def __init__(self, sizes, X, y, iters, learning_rate):
+    def __init__(self, sizes, X, y, iters=10, learning_rate=.1):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y,1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
+        self.biases = [np.random.randn(yi,1) for yi in sizes[1:]]
+        self.weights = [np.random.randn(yi,xi) for xi, yi in zip(sizes[:-1], sizes[1:])]
         self.X = X
         self.y = y
         self.gradient_descent(iters, learning_rate)
@@ -15,10 +15,20 @@ class Network(object):
     def cost_function(self, training):
         return sum((x[1]- self.predict(x[0])**2) for x in training)/(2*len(training))
 
-    def feedforward(self, a):
+    def predict(self, a):
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w,a)+b)
         return a
+
+    def feedforward(self, a):
+        zs = []
+        activations = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w,a)+b
+            a = sigmoid(z)
+            zs.append(z)
+            activations.append(a)
+        return zs, activations
 
     def gradient_descent(self, iters, learning_rate):
         for i in xrange(iters):
@@ -26,7 +36,8 @@ class Network(object):
                 self.back_prop(x, y)
 
     def back_prop(self, x, y):
-        pass
+        zs, activations = self.feedforward(x)
+
 
 def sigmoid(z):
     return 1/(1+np.exp(-z))
@@ -41,4 +52,4 @@ def reshape_y(in_ys):
 # TODO split into test and train data
 data = scipy.io.loadmat('data/ex4data1.mat')
 X, y = data['X'], reshape_y(data['y'])
-network = Network([X.shape[1], 25, 10])
+network = Network([X.shape[1], 25, 10], X, y)
